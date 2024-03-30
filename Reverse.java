ab996e3b7657b4b04986b54079a2e11aa3602deb
Shri <igave.0217@gmail.com>
2024-03-30 15:50:48 +0530

    Updated

--- Reverse.java
+++ Reverse.java
@@ -11,8 +11,8 @@ class Reverse {
     }
 
     public static void main(String[] args) {
-       int[] nums = {2, 3, 5, 4, 1};
-     // int [] nums = {1,2,3,4,5,6,7,8,9,10};
+     //  int[] nums = {2, 3, 5, 4, 1};
+      int [] nums = {1,2,3,4,5,6,7,8,9,10};
         reverseArray(nums);
         for (int i = 0; i < nums.length; i++) {
             System.out.print(nums[i]);
